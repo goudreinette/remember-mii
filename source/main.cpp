0acea51d8bb25c371b7987f2d21f390a3b7af76c
Rein van der Woerd <reinvanderwoerd@me.com>
2026-05-23 15:45:03 +0200

    reorganize main

--- source/main.cpp
+++ source/main.cpp
@@ -6,18 +6,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <wiiuse/wpad.h>
+
 #include <format>
 #include <iostream>
 #include <string>
 #include <vector>
 
-
 // Includes
 #include "colors.hpp"
+#include "input.hpp"
+#include "interface.hpp"
 #include "math.hpp"
 #include "random.hpp"
-#include "interface.hpp"
-
 
 // Images
 #include "remember_mii_jpg.h"
@@ -27,18 +27,17 @@
 
 // Scenes
 #include "scene.hpp"
-#include "scene_title.hpp"
-#include "scene_lostkeycard.hpp"
-#include "scene_lettertomunincipality.hpp"
-#include "scene_ending.hpp"
-#include "scene_buyingacar.hpp"
+#include "scene_biking.hpp"
 #include "scene_breakingupwithfriend.hpp"
+#include "scene_buyingacar.hpp"
 #include "scene_cleaningmachine.hpp"
+#include "scene_ending.hpp"
+#include "scene_golfclass.hpp"
 #include "scene_golfmurder.hpp"
 #include "scene_intro.hpp"
-#include "scene_golfclass.hpp"
-#include "scene_biking.hpp"
-
+#include "scene_lettertomunincipality.hpp"
+#include "scene_lostkeycard.hpp"
+#include "scene_title.hpp"
 
 // Random
 using Random = effolkronium::random_static;
@@ -47,16 +46,18 @@ using Random = effolkronium::random_static;
 int width;
 int height;
 
-
 int main() {
   // Initialize GRRLIB and WiiMote
   GRRLIB_Init();
-  WPAD_Init();
-  WPAD_SetDataFormat(WPAD_CHAN_0, WPAD_FMT_BTNS_ACC_IR);
 
   // Set width and height
   width = rmode->viWidth;
   height = rmode->viHeight;
+
+  // Init input
+  WPAD_Init();
+  WPAD_SetDataFormat(WPAD_CHAN_0, WPAD_FMT_BTNS_ACC_IR);
+  WPAD_SetVRes(0, width, height);
 
   // Load font
   textbox::load_font_texture();
@@ -65,41 +66,39 @@ int main() {
   ASND_Init();
   MP3Player_Init();
 
-  WPAD_SetVRes(0, width, height);
-
   Scene current_scene = Scene::GolfClass;
   Scene next_scene;
   cursor::load();
-  
+
   while (true) {
     if (current_scene == Scene::Title) {
-		next_scene = scene_title();
-	} else if (current_scene == Scene::Intro) {
-		next_scene = scene_intro();
-	} else if (current_scene == Scene::BuyingACar) {
-		next_scene = scene_buyingacar();
-	} else if (current_scene == Scene::BreakingUpWithAFriend) {
-		next_scene = scene_breakingupwithafriend();
-	} else if (current_scene == Scene::LetterToMunincipality) {
-		next_scene = scene_lettertomunincipality();
-	} else if (current_scene == Scene::LostKeyCard) {
-		next_scene = scene_lostkeycard();
-	} else if (current_scene == Scene::CleaningMachine) {
-		next_scene = scene_cleaningmachine();
-	} else if (current_scene == Scene::GolfClass) {
-		next_scene = scene_golfclass();
-	} else if (current_scene == Scene::GolfMurder) {
-		next_scene = scene_golfmurder();
-	} else if (current_scene == Scene::Biking) {
-		next_scene = scene_biking();
-	} else if (current_scene == Scene::Ending) {
-		next_scene = scene_ending();
-	}
+      next_scene = scene_title();
+    } else if (current_scene == Scene::Intro) {
+      next_scene = scene_intro();
+    } else if (current_scene == Scene::BuyingACar) {
+      next_scene = scene_buyingacar();
+    } else if (current_scene == Scene::BreakingUpWithAFriend) {
+      next_scene = scene_breakingupwithafriend();
+    } else if (current_scene == Scene::LetterToMunincipality) {
+      next_scene = scene_lettertomunincipality();
+    } else if (current_scene == Scene::LostKeyCard) {
+      next_scene = scene_lostkeycard();
+    } else if (current_scene == Scene::CleaningMachine) {
+      next_scene = scene_cleaningmachine();
+    } else if (current_scene == Scene::GolfClass) {
+      next_scene = scene_golfclass();
+    } else if (current_scene == Scene::GolfMurder) {
+      next_scene = scene_golfmurder();
+    } else if (current_scene == Scene::Biking) {
+      next_scene = scene_biking();
+    } else if (current_scene == Scene::Ending) {
+      next_scene = scene_ending();
+    }
 
-	current_scene = next_scene;
+    current_scene = next_scene;
   }
 
-	// GRRLIB_FreeTexture(fontTexture);
-	GRRLIB_Exit();  // Be a good boy, clear the memory allocated by GRRLIB
-	return 0;
+  // GRRLIB_FreeTexture(fontTexture);
+  GRRLIB_Exit();  // Be a good boy, clear the memory allocated by GRRLIB
+  return 0;
 }
