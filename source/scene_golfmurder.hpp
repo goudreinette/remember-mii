500a9d31cea7ad9a64f9bd2e339fe05d1e36b236
Rein van der Woerd <reinvanderwoerd@me.com>
2026-05-25 02:32:26 +0200

    golfmurder fade in out

--- source/scene_golfmurder.hpp
+++ source/scene_golfmurder.hpp
@@ -18,47 +18,58 @@
 Scene scene_golfmurder() {
     music::play_serguhh_loop();
 
-    GRRLIB_texImg* bg = GRRLIB_LoadTexture(golfmurder_1_golfintro_jpg);
-
     int slide_i = 0;
+    float slide_opacity = 0;
 
-    bool show_text_julius = false;
-    bool show_whack = false;
+    int num_frames = 5;
+    const uint8_t (*frames[5])[] = {
+        &golfmurder_1_golfintro_jpg,
+        &golfmurder_1_5_text_julius_jpg,
+        &golfmurder_2_anger_jpg,
+        &golfmurder_3_whack_jpg,
+        &golfmurder_4_bloodyclub_jpg
+    };
+
     
-    
+    GRRLIB_texImg* frame = GRRLIB_LoadTexture(*frames[slide_i]);
+
+    bool transition_down = false;
+
+
     while (true) {
         controller mote = update_wiimote();
 
-        if (mote.a_pressed) {
-            slide_i++;
 
-            if (slide_i == 1) {
-                bg = GRRLIB_LoadTexture(golfmurder_1_5_text_julius_jpg);
-            }
+        if (transition_down) {
+            slide_opacity = lrp(slide_opacity, 0, slide_speed);
+            if (slide_opacity < 30) {
+                transition_down = false;
+                slide_i = slide_i + 1;
+                // GRRLIB_FreeTexture(frame);
 
-            if (slide_i == 2) {
-                bg = GRRLIB_LoadTexture(golfmurder_2_anger_jpg);
-            }
+                // Dialogue and choice logic comes here
+                // -----
+                
+                if (slide_i == num_frames) {
+                    return Scene::CleaningMachine;
+                }
 
-            if (slide_i == 3) {
-                bg = GRRLIB_LoadTexture(golfmurder_3_whack_jpg);
+                frame = GRRLIB_LoadTexture(*frames[slide_i]);
             }
-
-            if (slide_i == 4) {
-                bg = GRRLIB_LoadTexture(golfmurder_4_bloodyclub_jpg);
-            }
-
-            if (slide_i == 5) {
-                GRRLIB_FreeTexture(bg);
-                return Scene::CleaningMachine;
-            }
+        } else {
+            slide_opacity = lrp(slide_opacity, 255, slide_speed);
         }
 
+        if (mote.a_pressed) {
+            transition_down = true;
+        }
 
-        cursor::draw(mote.x, mote.y);
         music::check_loop();
 
-        GRRLIB_DrawImg(60, 0, bg, 0, 1., 1., RGBA(255,255,255,255));  // Draw a jpeg
+        GRRLIB_DrawImg(60, -20, frame, 0, 1, 1, RGBA(255,255,255, slide_opacity));  // Draw a jpeg
+        cursor::draw(mote.x, mote.y);
+
+        // GRRLIB_DrawImg(60, 0, bg, 0, 1., 1., RGBA(255,255,255,255));  // Draw a jpeg
         GRRLIB_Render();
     }
 }
